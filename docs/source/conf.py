7fd2a87f43f7e059c072e1c0754887a97590c006
Ronny Hansen <ronnyhan@gmail.com>
2020-04-08 15:44:55 +0200

    Adding template sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +46,14 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
+html_logo = 'img/logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/intro/held-webinars.rst
+++ docs/source/intro/held-webinars.rst
@@ -3,12 +3,12 @@ Held webinars
 
 
 Modern Identity for Developers - March 30th 2020
-----------------------------------------------
+------------------------------------------------
 
 Link to PDF
 
 
 Modern Identity for Developers - April 6th 2020
-----------------------------------------------
+-----------------------------------------------
 
 Link to PDF
