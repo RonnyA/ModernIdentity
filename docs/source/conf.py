bebbf1467ea0e37eb5ffd72a9a5a4c068db91491
Ronny Hansen <ronnyhan@gmail.com>
2020-06-22 09:32:00 +0200

    updated version to 1.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2020 Microsoft'
 author = 'Ronny.Hansen@Microsoft.com'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.7'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
